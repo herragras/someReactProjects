facdffa1011b3d1fdd61f2a4808111c01e2aaf01
herragras <herragras@users.noreply.github.com>
2021-02-20 23:13:02 +0100

    homepage eklendi

--- src/index.js
+++ src/index.js
@@ -5,23 +5,26 @@ import MainComponent from "./TodoList/MainComponent";
 
 
 
-function getTime() {
-    return (new Date()).toLocaleTimeString()
-};
-
-const App = () => {
+class App extends React.Component {
+    state = {time:new Date().toLocaleTimeString()}
+    componentDidMount() {
+        setInterval(() => {this.setState({time:new Date().toLocaleTimeString()})
+        }, 1000)
+    }
+    render () {
     return (
         <div className="container mt-md-3">
             <div className="row justify-content">
                 <div className="col-5 align-items-center">
                     <h2 className="bg-secondary text-white p-3 text-center">Current Time:</h2>
-                    <h1 className="text-center">{getTime()}</h1>
+                    <h1 className="ml-md-3">{this.state.time}</h1>
                 </div>
             </div>
         </div>
 
     );
 };
+}
 ReactDOM.render(
             <App />, document.querySelector('#root'));
 
